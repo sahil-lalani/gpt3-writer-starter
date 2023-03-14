f4fcde5048406f46063b75b8c47afe70db62523a
Sahil Lalani <sahil.lalani83@gmail.com>
2023-03-13 20:14:12 -0400

    latest build

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
+        <meta property="og:title" content="tronald dump" key="title"/>
         <meta property="og:description" content="build with buildspace" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://tronalddump.io/img/tronalddump_850x850.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
